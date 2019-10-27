677826aec4c13f9a1c3c676ba827b74061528884
whiteGloom <karabin.m@yandex.ru>
2019-10-27 18:44:32 +0700

    v1.2.29 patch
    Список изменений:
    * - модуль "helper" перенесён на класс;

--- src/helper.js
+++ src/helper.js
@@ -1,24 +1,29 @@
-const helper = new function() {
-	this.isNumber = function(value) {
+class Helper {
+	constructor() {}
+
+	isNumber(value) {
 		if ((typeof value === "number" || typeof value === "string") & isFinite(value)) return true;
 
 		return false;
-	};
+	}
 
-	this.toNumber = function(value) {
+	toNumber(value) {
 		if (this.isNumber(value)) return +value;
-		return false;
-	};
 
-	this.isDOMEl = function(el) {
+		return false;
+	}
+
+	isDOMEl(el) {
 		if (this.isObject(el) && el.constructor !== Object && this.isNumber(el.nodeType) && +el.nodeType === 1) return true;
-		return false;
-	};
 
-	this.isObject = function(el) {
+		return false;
+	}
+
+	isObject(el) {
 		if (typeof el === "object" && el !== null) return true;
-		return false;
-	};
-};
 
-export default helper;
+		return false;
+	}
+}
+
+export default new Helper();
