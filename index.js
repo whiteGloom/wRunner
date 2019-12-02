ff905aa5db5135f6da3fd83800de34a348212019
whiteGloom <karabin.m@yandex.ru>
2019-12-02 22:13:30 +0700

    v1.8.4 patch
    Список изменений:
    • содержимое папки prod вынесено в отдельную ветку;
    
    https://github.com/whiteGloom/wRunner/issues/16

--- index.js
+++ index.js
@@ -50,8 +50,12 @@ webpackLoader.addToDevServerConfig({
 });
 
 function ghPagesInit() {
-  ghpages.publish('docs', () => {
+  ghpages.publish('docs', (err) => {
     console.log('GitHub Pages branch updated.');
+  });
+  ghpages.publish('prod', { branch: 'Production' }, (err) => {
+    if(err) console.log(err)
+    console.log('Prod branch updated.');
   });
 }
 
